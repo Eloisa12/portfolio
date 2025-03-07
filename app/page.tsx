9110d384ee88f7b3f10cd4afb684fed297b0528e
Eloisa Hong <jingjun.hong@student.unsw.edu.au>
2025-03-08 01:44:39 +1100

    styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import type { AppProps } from 'next/app'
+
 export default function Home() {
   return (
     <main>
-      <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
+      <h1 style={{color: "red"}} className="text-3xl font-bold underline">Hello, Next.js!</h1>
     </main>
   );
 }
