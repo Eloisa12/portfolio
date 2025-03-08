5bedbe307742cd37a11adbfc0614ec84d59b6799
Eloisa Hong <jingjun.hong@student.unsw.edu.au>
2025-03-09 04:10:14 +1100

    fixed a responsive issue

--- components/Stairs.tsx
+++ components/Stairs.tsx
@@ -2,12 +2,12 @@
 
 export default function Stairs() {
   return (
-    <div className="w-[40vw] h-[50vh] fixed left-0 top-120 flex">
-        <div className="flex flex-wrap justify-evenly  w-full h-full">
+    <div className="w-[45vw] h-[40vh] fixed left-0 bottom-0 flex">
+        <div className="flex flex-wrap justify-evenly w-full h-full">
         {[...Array(8)].map((_, i) => (
           <div
             key={i}
-            className="flex-1 w-15 h-90 border-r-12 border-t-12 border-midnight"
+            className="flex-1 border-r-12 border-t-12 border-midnight"
           />
         ))}
       </div>
